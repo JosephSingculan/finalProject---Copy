de23eb6fb57551be49ef9c9080803f80b16bc1af
JosephSingculan <133953808+JosephSingculan@users.noreply.github.com>
2024-01-10 16:16:16 +0800

    Update PackBase.js

--- PackBase.js
+++ PackBase.js
@@ -9,16 +9,16 @@ var imgs = ["osmena kawasan.png",
             "fort pedro taoist sumilon.png",
             "cebu saffari kota beach omageica mangrove.png"];
 var packs = [
-    { name: "Pack 1", description: "Osmena Peak - Kawasan Falls",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:69000,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
-    { name: "Pack 2", description: "Badian Canyoneering - Oslob Whale Shark",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}]  },
-    { name: "Pack 3", description: "Sirao Garden - Moalboal Sardines",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 4", description: "Museo Oslob - Sumilon Sandbar",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
-    { name: "Pack 5", description: "Tumalog Falls- Mainit Dive",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 6", description: "Mainit Spring - Oslob Whale Shark - Sirao Garden",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 7", description: "Bay Bay beach - Moalboal Sardines - Busay Cave",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 8", description: "Badian Canyoneering - Temple of Leah - Kawasan",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 9", description: "Fort San Pedro - Taoist Temple - Sumilong Sandbar",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"}]},
-    { name: "Pack 10", description: "Cebu Saffari - Kota Beach - Omageica Mangrove",LongDescription:["Nice Views","Big COCONUTS","really nice COCONUTS"],price:1234,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] }
+    { name: "Pack 1", description: "Osmena Peak - Kawasan Falls",LongDescription:["Site picture taking","Canyoneering","Cliff diving"],price:"Php. 1,500 per head",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
+    { name: "Pack 2", description: "Badian Canyoneering - Oslob Whale Shark",LongDescription:["Canyoneering","Cliff diving","Whale shark waitching"],price:"Php. 1,500 per head",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}]  },
+    { name: "Pack 3", description: "Sirao Garden - Moalboal Sardines",LongDescription:["Site picture taking","Diving","Swimming with Sardines"],price:"Php. 1,500 per head",reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 4", description: "Museo Oslob - Sumilon Sandbar",LongDescription:["Site picture taking","Island Hopping","Swimming"],price:"Php. 1,500 per head",reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] },
+    { name: "Pack 5", description: "Tumalog Falls- Mainit Dive",LongDescription:["Swimming","Diving","Site picture taking"],price:"Php. 1,500 per head",reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 6", description: "Mainit Spring - Oslob Whale Shark - Sirao Garden",LongDescription:["Site picture taking","Whale Shark spotting","Swimming"],price:"Php. 1,850 per head",reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 7", description: "Bay Bay beach - Moalboal Sardines - Busay Cave",LongDescription:["Swimming","Sardines spotting","Cave Exploring"],price:"Php. 1,850 per head",reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 8", description: "Badian Canyoneering - Temple of Leah - Kawasan",LongDescription:["Cnayoneering","Site picutre taking","Cliff diving"],price:"Php. 1,850 per head",reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 9", description: "Fort San Pedro - Taoist Temple - Sumilong Sandbar",LongDescription:["Site picutre taking","Swimming","Snorkelling"],price:"Php. 1,850 per head",reviews:[{star: 5,description:"very nice"}]},
+    { name: "Pack 10", description: "Cebu Saffari - Kota Beach - Omageica Mangrove",LongDescription:["Site picture taking","Swimming","Kayaking"],price:1,reviews:[{star: 5,description:"very nice"},{star: 4,description:"very nice"}] }
 ];
 var keyword = "";
 
